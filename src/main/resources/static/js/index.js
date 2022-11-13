7692985ee5adb9a662c25ea9560905827a817cf9
Allison <Violinagin@gmail.com>
2022-11-12 22:31:22 -0800

    added filename and programname

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,22 +1,21 @@
 console.log("This is the JS file")
+const programInput = document.getElementById('program-name')
 const exerciseInput = document.getElementById('exercise-name')
 const weightInput = document.getElementById('weight')
 const repsInput = document.getElementById('reps')
 const setsInput = document.getElementById('sets')
 const buttonSubmit = document.getElementById('submit')
 
-const str = exerciseInput.value + "," + weightInput.value + "," + repsInput.value + "," + setsInput.value;
-
 buttonSubmit.addEventListener("click", ()=>{
     const str = exerciseInput.value + "," + weightInput.value + "," + repsInput.value + "," + setsInput.value;
     console.log(str)
     var downloadLink = document.createElement("a");
     var csv = str
-    var blob = new Blob(["\ufeff", csv]);
+    var blob = new Blob(["\ufeff", csv], {type: 'text/csv'});
     var url = URL.createObjectURL(blob);
+    const exportFilename = programInput.value;
     downloadLink.href = url;
-    downloadLink.download = "data.csv";
-
+    downloadLink.setAttribute('download', exportFilename);
     document.body.appendChild(downloadLink);
     downloadLink.click();
     document.body.removeChild(downloadLink);
