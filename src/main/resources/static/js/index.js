49f0a2a2bdec6b93d5be925d2478d7ad0d8b0e46
Allison <Violinagin@gmail.com>
2022-11-13 14:12:17 -0800

    added multiple exercise lines on screen and csv file

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -6,22 +6,37 @@ const setsInput = document.getElementById('sets')
 const buttonSubmit = document.getElementById('submit')
 const buttonAdd = document.getElementById('add-exercise')
 const dayExercise = document.getElementById('day-exercise')
+const fullProgram =document.getElementById('full-program')
 const programArray = []
 
+let programInputValue = ""
+programInput.addEventListener("change",(e) =>{
+    programInputValue = e.target.value;
+    fullProgram.innerHTML = programInputValue;
+})
 
 buttonAdd.addEventListener("click", ()=>{
-    const exerciseObject = {exercise:exerciseInput.value,weight:weightInput.value,reps:repsInput.value,sets:setsInput.value};
+    const exerciseObject = {
+        exercise:exerciseInput.value,
+        weight:weightInput.value,
+        reps:repsInput.value,
+        sets:setsInput.value
+    };
     programArray.push(exerciseObject);
     console.log(programArray);
     let textofArray = "";
-    for(let i in programArray) {
-        textofArray += programArray[i].name;
+    for(let i=0;i< programArray.length; i++) {
+        textofArray += "<p>exercise:" + programArray[i].exercise + " weight:" + programArray[i].weight +" reps:"+ programArray[i].reps +" sets:"+ programArray[i].sets+"</p>";
     }
-    dayExercise.innerHTML += "<p>" + textofArray;
+    dayExercise.innerHTML = textofArray
 })
 
 buttonSubmit.addEventListener("click", ()=>{
-    const str = exerciseInput.value + "," + weightInput.value + "," + repsInput.value + "," + setsInput.value;
+    //const str = exerciseInput.value + "," + weightInput.value + "," + repsInput.value + "," + setsInput.value;
+    let str = "";
+    for(let i=0;i< programArray.length; i++) {
+        str += programArray[i].exercise + "," + programArray[i].weight + "," + programArray[i].reps + "," + programArray[i].sets + "\n";
+    }
     const downloadLink = document.createElement("a");
     const csv = str
     const blob = new Blob(["\ufeff", csv], {type: 'text/csv'});
