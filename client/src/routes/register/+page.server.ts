af83db57765838d613327e03254510523cc7d973
sg55kg <sdollar1@gmail.com>
2022-11-26 13:18:11 -0600

    Added form validation to register form

--- client/src/routes/register/+page.server.ts
+++ client/src/routes/register/+page.server.ts
@@ -1,12 +1,69 @@
 import type {Action, Actions} from "@sveltejs/kit";
 import {redirect} from "@sveltejs/kit";
+import {z} from 'zod'
+import type {ZodError} from 'zod'
 
+const registerSchema = z.object({
+    username: z
+        .string({ required_error: 'Username is required' })
+        .min(3, { message: 'Username must be at least 3 characters' })
+        .max(30)
+        .trim(),
+    password: z
+        .string({ required_error: 'Password is required' })
+        .min(8, { message: 'Password must be at least 8 characters' }),
+    passwordConfirm: z
+        .string({ required_error: 'Password confirmation is required' }),
+    email: z
+        .string({ required_error: 'Email is required' })
+        .min(1)
+        .max(50)
+        .email({ message: 'Must be a valid email address' }),
+    firstName: z
+        .string({ required_error: 'First name is required' })
+        .min(1)
+        .max(50)
+        .trim(),
+    lastName: z
+        .string({ required_error: 'Last name is required' })
+        .min(1)
+        .max(50)
+        .trim()
+}).superRefine(({ password, passwordConfirm }, ctx) => {
+    if (password !== passwordConfirm) {
+        ctx.addIssue({
+            code: 'custom',
+            message: 'Passwords must be matching',
+            path: ['passwordConfirm']
+        })
+    }
+})
+
+interface RegisterData extends FormData {
+    username: string,
+    password: string,
+    passwordConfirm: string,
+    email: string,
+    firstName: string,
+    lastName: string,
+}
 
 export const actions: Actions = {
     default: async ({ request }) => {
 
-        const data = await request.formData()
-
+        const data = Object.fromEntries(await request.formData())
+        try {
+            const validated = registerSchema.parse(data)
+        } catch (e: any) {
+            console.log(e.errors[0].path)
+            const { fieldErrors: errors } = (e as ZodError).flatten()
+            const { password, passwordConfirm, ...rest } = data
+            console.log(errors)
+            return {
+                data: rest,
+                errors
+            }
+        }
         try {
             const res = await fetch(`http://localhost:8180/auth/register`, {
                 method: 'POST',
@@ -14,16 +71,14 @@ export const actions: Actions = {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    username: data.get('username'), //username,
-                    password: data.get('password'),
-                    firstName: data.get('first-name'),
-                    lastName: data.get('last-name'),
-                    email: data.get('email'),
+                    username: data['username'], //username,
+                    password: data['password'],
+                    firstName: data['first-name'],
+                    lastName: ['last-name'],
+                    email: data['email'],
                     role: 'athlete'
                 })
-
             })
-
         } catch (e) {
             return { error: e}
         }
--- client/src/routes/register/+page.svelte
+++ client/src/routes/register/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import {enhance} from '$app/forms'
-
+    export let form
 </script>
 
 <form method="POST" use:enhance={() => {
@@ -9,16 +9,74 @@
     }
 }}>
 
-    <div class="bg-white border-white p-4 px-8 flex flex-col justify-items-center">
-        <h1 class="font-extrabold text-3xl pb-2">Register</h1>
-        <input name="username" type="text" placeholder="Username" class="register-input">
-        <input name="password" type="password" placeholder="Password" class="register-input">
-        <input name="first-name" type="text" placeholder="First Name" class="register-input">
-        <input name="last-name" type="text" placeholder="Last Name" class="register-input">
-        <input name="email" type="text" placeholder="Email Address" class="register-input">
-        <button class="m-auto mt-6 p-2 bg-yellow-400 hover:bg-yellow-600">
+    <div class="bg-white border-white p-4 px-8 flex flex-col justify-items-center content-center">
+        <h1 class="font-extrabold text-3xl pb-2">
+            Register
+        </h1>
+        <div class="flex justify-center flex-col m-auto">
+            <input name="username"
+                   type="text"
+                   placeholder="Username"
+                   class={!form?.errors?.username ? 'register-input' : 'input-error'}
+                   autocomplete="off"
+                   value={form?.data?.username ?? ''}>
+            {#if form?.errors?.username}
+                <small class="label-text-alt text-red-500">{form?.errors?.username[0]}</small>
+            {/if}
+        </div>
+        <div class="flex justify-center flex-col m-auto">
+            <input name="password"
+                   type="password"
+                   placeholder="Password"
+                   class={!form?.errors?.password ? 'register-input' : 'input-error'}>
+            {#if form?.errors?.password}
+                <small class="label-text-alt text-red-500">{form?.errors?.password[0]}</small>
+            {/if}
+        </div>
+        <div class="flex justify-center flex-col m-auto">
+            <input name="passwordConfirm"
+                   type="password"
+                   placeholder="Confirm Password"
+                   class={!form?.errors?.passwordConfirm ? 'register-input' : 'input-error'}>
+            {#if form?.errors?.passwordConfirm}
+                <small class="label-text-alt text-red-500">{form?.errors?.passwordConfirm[0]}</small>
+            {/if}
+        </div>
+        <div class="flex justify-center flex-col m-auto">
+            <input name="firstName"
+                   type="text"
+                   placeholder="First Name"
+                   class={!form?.errors?.firstName ? 'register-input' : 'input-error'}
+                   value={form?.data?.firstName ?? ''}>
+            {#if form?.errors?.firstName}
+                <small class="label-text-alt text-red-500">{form?.errors?.firstName[0]}</small>
+            {/if}
+        </div>
+        <div class="flex justify-center flex-col m-auto">
+            <input name="lastName"
+                   type="text"
+                   placeholder="Last Name"
+                   class={!form?.errors?.lastName ? 'register-input' : 'input-error'}
+                   value={form?.data?.lastName ?? ''}>
+            {#if form?.errors?.lastName}
+                <small class="label-text-alt text-red-500">{form?.errors?.lastName[0]}</small>
+            {/if}
+        </div>
+        <div class="flex justify-center flex-col m-auto">
+            <input name="email"
+                   type="text"
+                   placeholder="Email Address"
+                   class={!form?.errors?.email ? 'register-input' : 'input-error'}
+                   value={form?.data?.email ?? ''}>
+            {#if form?.errors?.email}
+                <small class="label-text-alt text-red-500">{form?.errors?.email[0]}</small>
+            {/if}
+        </div>
+
+        <button class="m-auto my-6 p-2 bg-yellow-400 hover:bg-yellow-600">
             Sign Up
         </button>
+        <hr>
         <small class="m-auto pt-4 font-semibold">
             Already have an account? <a class="text-teal-500" href="/login">Login</a>
         </small>
@@ -27,13 +85,24 @@
 
 <style>
     .register-input {
-        padding: 1em;
-        border-bottom: 3px solid black;
+        border-bottom: 3px solid gray;
         transition: border-bottom-color .25s linear;
+        padding: 1em 2em .5em;
+        font-size: 18px;
     }
 
     .register-input:focus {
         border-bottom: 3px solid deepskyblue;
+        outline: none;
+    }
+
+    .input-error {
+        border-bottom: 3px solid red;
+        padding: 1em 2em .5em;
+        font-size: 18px;
+    }
+
+    .input-error:focus {
         outline: none;
     }
 </style>
