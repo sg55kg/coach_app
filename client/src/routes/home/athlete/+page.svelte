36173d6c4a5245f0e077386aec15599ea208dc3c
Allison <Violinagin@gmail.com>
2023-01-13 19:46:13 -0800

    Athlete program empty style for mobile and web

--- client/src/routes/home/athlete/+page.svelte
+++ client/src/routes/home/athlete/+page.svelte
@@ -40,12 +40,12 @@
 
 <div class="flex flex-col">
     <div class="sm:text-center md:text-center lg:text-start m-2 flex justify-center lg:justify-start">
-        <h1 class="font-bold text-2xl w-fit">Welcome {$userDB?.username}</h1>
+        <h1 class="font-bold text-xl w-fit">Welcome {$userDB?.username}</h1>
     </div>
     <div class="lg:m-4">
         <div class="flex flex-col items-center lg:items-start">
-            <h3 class="text-xl font-bold">My Program</h3>
-            <h5 class="text-xl">{dayjs().format('dddd, MMMM D')}</h5>
+            <h3 class="text-2xl font-bold tracking-wider">My Program</h3>
+            <h5 class="my-3 text-xl">{dayjs().format('dddd, MMMM D')}</h5>
         </div>
 
 
@@ -60,7 +60,7 @@
                 Rest Day
             </div>
         {:else}
-            <div>
+            <div class="py-4 text-center md:text-left">
                 No programming available for today
             </div>
         {/if}
@@ -69,14 +69,14 @@
         </div>
 
     </div>
-    <hr class="m-2 mx-24">
+    <hr class="hidden md:flex m-2 mx-24">
     <div class="m-4 flex flex-col">
         {#if $userDB?.athleteData?.team}
             <h4 class="text-2xl text-center">{$userDB.athleteData.team.name}</h4>
             <a class="text-lg underline text-link text-center" href="/home/athlete/team">View</a>
             <a class="text-lg underline text-link text-center" href="/home/athlete/teams">Browse Teams</a>
         {:else}
-            <a class="text-lg underline text-link" href="/home/athlete/teams">Join a team</a>
+            <a class="text-lg text-center md:text-left underline text-link" href="/home/athlete/teams">Join a team</a>
         {/if}
     </div>
 </div>
