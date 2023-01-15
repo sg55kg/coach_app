b3fe4bfe53791525c0906c7240061bd2b786f682
sg55kg <sdollar1@gmail.com>
2023-01-14 19:09:27 -0600

    Fixed undefined error

--- client/src/routes/home/athlete/+page.svelte
+++ client/src/routes/home/athlete/+page.svelte
@@ -17,7 +17,7 @@
 
 <div class="flex flex-col">
     <div class="sm:text-center md:text-center lg:text-start m-2 flex justify-center lg:justify-start">
-        <h1 class="font-bold text-xl w-fit">Welcome {$userDB?.username}</h1>
+        <h1 class="font-bold text-xl w-fit">Welcome {$userDB?.username ? $userDB.username : ''}</h1>
     </div>
     <div class="lg:m-4">
         <div class="flex flex-col items-center lg:items-start">
@@ -26,13 +26,13 @@
         </div>
 
 
-        {#if $currentDay && $currentDay.isRestDay === false && $currentDay.exercises.length > 0}
+        {#if $currentDay && $currentDay?.isRestDay === false && $currentDay?.exercises?.length > 0}
             <div class="lg:m-4 flex flex-col justify-center lg:p-5 sm:p-2 md:p-2">
                 {#each $currentDay.exercises as exercise, index (index)}
                     <IncompleteExercise bind:exercise={exercise} />
                 {/each}
             </div>
-        {:else if $currentDay && $currentDay.isRestDay}
+        {:else if $currentDay && $currentDay?.isRestDay}
             <div>
                 Rest Day
             </div>
@@ -49,7 +49,7 @@
     <hr class="hidden md:flex m-2 mx-24">
     <div class="m-4 flex flex-col">
         {#if $userDB?.athleteData?.team}
-            <h4 class="text-2xl text-center">{$userDB.athleteData.team.name}</h4>
+            <h4 class="text-2xl text-center">{$userDB?.athleteData?.team?.name}</h4>
             <a class="text-lg underline text-link text-center" href="/home/athlete/team">View</a>
             <a class="text-lg underline text-link text-center" href="/home/athlete/teams">Browse Teams</a>
         {:else}
