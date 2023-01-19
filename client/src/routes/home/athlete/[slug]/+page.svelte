bb153bcef3a5dde74243dddf5f8915a95010235c
sg55kg <sdollar1@gmail.com>
2023-01-19 00:10:43 -0600

    Removed frontend fetch on records page

--- client/src/routes/home/athlete/[slug]/+page.svelte
+++ client/src/routes/home/athlete/[slug]/+page.svelte
@@ -1,34 +1,10 @@
 <script lang="ts">
-    import {AthleteData} from "$lib/classes/user";
-    import {onMount} from "svelte";
-    import UserService from "$lib/service/userService";
-    import {auth0Client} from "$lib/stores/authStore";
+    import {userDB} from "$lib/stores/authStore";
     import ProgramStats from "$lib/components/Stats/ProgramStats.svelte";
-
-    let athlete: AthleteData
-    let loadingAthleteData: boolean = true
-
-    onMount(async () => {
-        loadingAthleteData = true
-        const pathArr = window.location.pathname.split('/')
-        const athleteId = pathArr[pathArr.length-1]
-
-        try {
-            const athleteRes: AthleteData = await UserService.fetchAthleteData($auth0Client!, athleteId)
-            athlete = athleteRes
-        } catch (e) {
-            console.log(e)
-        }
-        loadingAthleteData = false
-    })
 </script>
 
-{#if athlete && !loadingAthleteData}
-    <ProgramStats athlete={athlete} />
-{:else if loadingAthleteData}
-    <div class="text-center font-semibold text-xl">
-        Fetching athlete stats...
-    </div>
+{#if $userDB.athleteData }
+    <ProgramStats athlete={$userDB.athleteData} />
 {:else}
     <div class="text-center font-semibold text-lg">
         Could not retrieve this athlete. Please try refreshing the page or navigating away and back.
